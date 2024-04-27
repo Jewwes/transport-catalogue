1e2a056bd50f6c8086feb8430dd6ff9e48d175cb
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-04-27 18:05:48 +0500

    stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,22 +1,60 @@
-#include <ostream>
 #include "transport_catalogue.h"
+#include <ostream>
+#include <iomanip>
 namespace TransportCatalogue {
 
-namespace detail {
+    namespace detail {
 
-        void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
-                               std::ostream& output) {
-            if (request.find("Bus") == 0) {
-                std::string route_name(request.substr(4)); 
-                transport_catalogue.GetRouteInfo(route_name);
-            } else if (request.find("Stop") == 0) {
-                std::string stop_name(request.substr(5)); 
-                transport_catalogue.GetStopInfo(stop_name);
+        void PrintRoute(std::string& line, TransportCatalogue& catalogue, std::ostream& out) {
+            std::string bus = line.substr(1, line.npos);
+            if (catalogue.FindRoute(bus)) {
+                out << "Bus " << bus << ": " << catalogue.GetRouteInfo(bus)->stops_count << " stops on route, "
+                    << catalogue.GetRouteInfo(bus)->unique_stops_count << " unique stops, " << std::setprecision(6)
+                    << catalogue.GetRouteInfo(bus)->route_length << " route length, "
+                    << catalogue.GetRouteInfo(bus)->curvature << " curvature\n";
             }
             else {
-                output << "Invalid request: " << request << std::endl;
+                out << "Bus " << bus << ": not found\n";
             }
         }
-} 
 
-} 
+        void PrintStop(std::string& line, TransportCatalogue& catalogue, std::ostream& out) {
+            std::string stop = line.substr(1, line.npos);
+            if (catalogue.FindStop(stop)) {
+                out << "Stop " << stop << ":";
+                auto stopInfo = catalogue.GetStopInfo(stop)->buses;
+                if (!stopInfo.empty()) {
+                    out << " buses ";
+                    for (const auto& sto : stopInfo) {
+                        out << sto << " ";
+                    }
+                    out << "\n";
+                }
+                else {
+                    out << " no buses\n";
+                }
+            }
+            else {
+                out << "Stop " << stop << ": not found\n";
+            }
+
+        }
+
+        void ParseAndPrintStat(std::istream& in, TransportCatalogue& catalogue, std::ostream& out) {
+            size_t requests_count;
+            in >> requests_count;
+            for (size_t i = 0; i < requests_count; ++i) {
+                std::string keyword, line;
+                in >> keyword;
+                std::getline(in, line);
+                if (keyword == "Bus") {
+                    PrintRoute(line, catalogue, out);
+                }
+                if (keyword == "Stop") {
+                    PrintStop(line, catalogue, out);
+                }
+            }
+        }
+    }
+
+}
