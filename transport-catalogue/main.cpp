3e552379c0da3a98ac2bc87975e392849798baf2
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-04-27 18:05:27 +0500

    main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "input_reader.h"
 #include "stat_reader.h"
 
 using namespace std;
+
 int main() {
     TransportCatalogue::TransportCatalogue catalogue;
-
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        TransportCatalogue::detail::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        TransportCatalogue::detail::ParseAndPrintStat(catalogue, line, cout);
-    }
+    TransportCatalogue::detail::InputReader::ReadInput(cin, catalogue);
+    TransportCatalogue::detail::ParseAndPrintStat(cin,catalogue, cout);
 }
