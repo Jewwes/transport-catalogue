69d10c4d49c942877adc888acf7ae5ee4dee2f4f
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-05-19 16:38:07 +0500

    Update request_handler.h

--- request_handler.h
+++ request_handler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "json.h"
+#include "json_reader.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
 
@@ -14,9 +15,15 @@ public:
         , renderer_(renderer)
     {
     }
-    std::optional<transport::BusStat> GetBusStat(const std::string_view& bus_number) const;
+
+    void ProcessRequests(const json::Node& stat_requests) const;
+
+    const json::Node PrintRoute(const json::Dict& request_map) const;
+    const json::Node PrintStop(const json::Dict& request_map) const;
+
+
     svg::Document RenderMap() const;
-    const transport::TransportCatalogue& GetCatalogue() const;
+
 private:
     const transport::TransportCatalogue& catalogue_;
     const map_renderer::MapRenderer& renderer_;
