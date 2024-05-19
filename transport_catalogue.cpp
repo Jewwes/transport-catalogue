f21f72e564a8cc761f936bb5817122e99259f92f
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-05-19 16:37:12 +0500

    Update transport_catalogue.cpp

--- transport_catalogue.cpp
+++ transport_catalogue.cpp
@@ -21,6 +21,40 @@ namespace transport {
         }
     }
 
+    std::optional<transport::BusStat> TransportCatalogue::GetBusStat(const std::string_view& bus_number) const {
+        transport::BusStat bus_stat{};
+        std::unordered_set<const transport::Stop*> unique_stops;
+        for (const auto* stop : FindRoute(bus_number)->stops) {
+            unique_stops.insert(stop);
+        }
+        size_t num_unique_stops = unique_stops.size();
+        const transport::Bus* bus = FindRoute(bus_number);
+        if (bus->is_circle) bus_stat.stops_count = bus->stops.size();
+        else bus_stat.stops_count = bus->stops.size() * 2 - 1;
+
+        int route_length = 0;
+        double geographic_length = 0.0;
+
+        for (size_t i = 0; i < bus->stops.size() - 1; ++i) {
+            auto from = bus->stops[i];
+            auto to = bus->stops[i + 1];
+            if (bus->is_circle) {
+                route_length += GetDistance(from, to);
+                geographic_length += geo::ComputeDistance(from->coordinates, to->coordinates);
+            }
+            else {
+                route_length += GetDistance(from, to) + GetDistance(to, from);
+                geographic_length += geo::ComputeDistance(from->coordinates, to->coordinates) * 2;
+            }
+        }
+
+        bus_stat.unique_stops_count = num_unique_stops;
+        bus_stat.route_length = route_length;
+        bus_stat.curvature = route_length / geographic_length;
+
+        return bus_stat;
+    }
+
     const std::map<std::string_view, const Bus*> TransportCatalogue::AllBuses() const {
         std::map<std::string_view, const Bus*> result{buses_name_.begin(),buses_name_.end()};
         return result;
