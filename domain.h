088a0e7ec49709dfdd746653ddab86127d90ddb8
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-05-19 11:34:08 +0500

    Update domain.h

---
+++ domain.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "geo.h"
+
+#include <string>
+#include <vector>
+#include <set>
+#include <unordered_map>
+
+
+namespace transport {
+
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+        std::set<std::string> buses_by_stop;
+    };
+
+    struct Bus {
+        std::string name;
+        std::vector<const Stop*> stops;
+        bool is_circle = false;
+    };
+
+    struct BusStat {
+        size_t stops_count;
+        size_t unique_stops_count;
+        double route_length;
+        double curvature;
+    };
+
+    struct StopStat {
+        std::string name;
+        std::set<std::string> buses;
+    };
+
+}
