177d1f136fd279556e3a3bdcdf7ae2f8bbe2b14c
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-05-19 11:35:27 +0500

    Update json_reader.h

---
+++ json_reader.h
@@ -0,0 +1,31 @@
+/*
+ * Здесь можно разместить код наполнения транспортного справочника данными из JSON,
+ * а также код обработки запросов к базе и формирование массива ответов в формате JSON
+ */
+
+#pragma once
+
+#include "json.h"
+#include "transport_catalogue.h"
+#include "request_handler.h"
+#include "map_renderer.h"
+
+#include <iostream>
+#include <sstream>
+class JsonReader {
+public:
+    JsonReader(std::istream& input);
+    const json::Node& GetRequest(std::string request) const;
+    void FillCatalogue(transport::TransportCatalogue& catalogue);
+    void ProcessRequests(const json::Node& requests, RequestHandler& dos) const;
+    const json::Node PrintStop(const json::Dict& request, RequestHandler& dos) const;
+    const json::Node PrintRoute(const json::Dict& request, RequestHandler& dos) const;
+    const json::Node PrintMap(const json::Dict& request, RequestHandler& dos) const;
+    map_renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
+private:
+    json::Document input_;
+
+    void FillStop(const json::Dict& request_map, transport::TransportCatalogue& catalogue) const;
+    void FillStopDistances(const json::Dict& request, transport::TransportCatalogue& catalogue) const;
+    void FillRoute(const json::Dict& request_map, transport::TransportCatalogue& catalogue) const;
+};
