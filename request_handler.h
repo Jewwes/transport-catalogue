a9023251457e614edd70a65cbf972c701a893fc4
Jewwes <144038777+Jewwes@users.noreply.github.com>
2024-05-26 03:19:10 +0500

    Update request_handler.h

--- request_handler.h
+++ request_handler.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "json.h"
-#include "json_reader.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
 
@@ -15,15 +14,8 @@ public:
         , renderer_(renderer)
     {
     }
-
-    void ProcessRequests(const json::Node& stat_requests) const;
-
-    const json::Node PrintRoute(const json::Dict& request_map) const;
-    const json::Node PrintStop(const json::Dict& request_map) const;
-
-
     svg::Document RenderMap() const;
-
+    const transport::TransportCatalogue& GetCatalogue() const;
 private:
     const transport::TransportCatalogue& catalogue_;
     const map_renderer::MapRenderer& renderer_;
